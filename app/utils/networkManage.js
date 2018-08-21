b7855ef0ccc3ee342d7d493e0c1e2cf990d990bf
zhaoyiyu <961839401@qq.com>
2018-08-21 13:12:48 +0800

    ignore

--- app/utils/networkManage.js
+++ app/utils/networkManage.js
@@ -184,7 +184,7 @@ export default class networkManage {
             from: store.getState().Core.walletAddress,
             to: toAddress,
             value:value,
-            gas:transactionGasPrice,
+            gas:gasLimit,
             gasPrice:transactionGasPrice,
         };
         console.log(transactionConfig);
