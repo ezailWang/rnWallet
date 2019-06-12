564749281bb3e202a374f9340610470673ad5672
jellywang <ezail.shandong@gmail.com>
2019-06-12 16:41:21 +0800

    fix fontfamily fontWeight
    Signed-off-by: jellywang <ezail.shandong@gmail.com>

--- app/App.js
+++ app/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { Text, TextInput } from 'react-native';
+import { Text, TextInput, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import lodash from 'lodash';
 import store from './config/store/ConfigureStore';
 import Navigator from './navigation';
 import Analytics from './utils/Analytics';
 
 TextInput.defaultProps = Object.assign({}, TextInput.defaultProps, { allowFontScaling: false });
 Text.defaultProps = Object.assign({}, Text.defaultProps, { allowFontScaling: false });
+
+// 给全局文本组件添加空字体，fix在某些android机型上，添加fontWeight导致字体隐藏的问题
+const styles = StyleSheet.create({
+  defaultFontFamily: {
+    fontFamily: 'system font',
+  },
+});
+
+Text.render = lodash.wrap(Text.render, (func, ...args) => {
+  const originText = func.apply(this, args);
+  return React.cloneElement(originText, {
+    style: [originText.props.style, styles.defaultFontFamily],
+  });
+});
 
 function getCurrentRouteName(navigationState) {
   if (!navigationState) {
